17d8e415b1eaa6d9d09f0d1b4abe04f7723ce5f3
Oleg <brtnchk@gmail.com>
2020-12-03 11:09:18 +0200

    comments

--- PropertyContainer/PropertyContainer.php
+++ PropertyContainer/PropertyContainer.php
@@ -4,21 +4,37 @@ class PropertyContainer implements PropertyContainerInterface
 {
     private $propertyContainer = [];
 
+    /**
+     * @param $propertyName
+     * @param $value
+     */
     public function addProperty($propertyName, $value)
     {
         $this->propertyContainer[$propertyName] = $value;
     }
 
+    /**
+     * @param $propertyName
+     */
     public function deleteProperty($propertyName)
     {
         unset($this->propertyContainer[$propertyName]);
     }
 
+    /**
+     * @param $propertyName
+     * @return mixed|null
+     */
     public function getProperty($propertyName)
     {
         return $this->propertyContainer[$propertyName] ?? null;
     }
 
+    /**
+     * @param $propertyName
+     * @param $value
+     * @throws Exception
+     */
     public function setProperty($propertyName, $value)
     {
         if (!isset($this->propertyContainer[$propertyName])) {
